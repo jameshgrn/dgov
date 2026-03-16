4e19dcace104473b7f480a060e83a7bd3a80f8fe
Jake Gearon <jake.gearon@gmail.com>
2026-03-16 13:44:51 -0400

    Merge fix-ansi-strip

--- src/dgov/status.py
+++ src/dgov/status.py
@@ -345,11 +345,13 @@ def capture_worker_output(
 
 _ANSI_RE = re.compile(
     r"\x1b\[[0-9;?]*[a-zA-Z]"  # CSI sequences (cursor, color, etc.)
-    r"|\x1b\].*?(?:\x07|\x1b\\)"  # OSC sequences (title, hyperlinks)
+    r"|\x1b\].*?(?:\x07|\x1b\\)"  # OSC sequences (title, hyperlinks, cwd)
+    r"|\x1bk.*?\x1b\\\\"  # tmux title-setting (ESC k ... ESC\)
     r"|\x1b\[.*?m"  # SGR color codes
     r"|\x1b[()][0-9A-Za-z]"  # Character set selection
     r"|\x1b[=>]"  # Keypad modes
     r"|[\x00-\x08\x0e-\x1a\x7f]"  # Other control characters
+    r"|\r"  # Carriage returns
 )
 
 
